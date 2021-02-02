400061d7ddf0216025d724efc45c65f710e25ecd
Serhii Horobets <serhii.horobets82@gmail.com>
2021-02-02 23:57:13 +0200

    Fixed warning

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
+use serde_derive::{Deserialize, Serialize};
 use std::fs::{read_dir, File};
-use std::io::{self, BufRead, };
-use std::path::Path;
 use std::io::prelude::*;
 use std::io::BufWriter;
-use serde_derive::{Deserialize, Serialize};
+use std::io::{self, BufRead};
+use std::path::{Path, PathBuf};
 
-static LOG_DIR: &str = "."; // "C:/Projects/log_parser/";
-
+static LOG_DIR: &str = ".";
 
 #[derive(Serialize, Deserialize)]
 struct Log {
-    text: String
+    text: String,
 }
 
 fn main() {
@@ -20,23 +19,29 @@ fn main() {
             for entry in entries {
                 let path = entry.unwrap().path();
                 if path.is_file() && path.extension().unwrap_or_default() == "log" {
-                    println!("File :: {:?}, [{:?}]", path, path.join(".txt"));
-                    let file = File::create(path.with_extension("log.txt")).expect("Can't create output file");
-                    let mut f = BufWriter::new(file);
-                    
-                    if let Ok(lines) = read_lines(path) {
-                        for line in lines {
-                            if let Ok(log_line) = line {
-                                let log: Log = serde_json::from_str(&log_line).expect("JSON was not well-formatted");
-                                writeln!(f, "{}", log.text.trim());
-                            }
-                        }
-                    }
+                    parse_file(path);
                 }
             }
         }
     }
     return;
+}
+
+fn parse_file(path: PathBuf) {
+    println!("Processing file {:?}", path);
+    let file = File::create(path.with_extension("log.txt")).expect("Can't create output file");
+    let mut writer = BufWriter::new(file);
+    if let Ok(lines) = read_lines(path) {
+        for line in lines {
+            if let Ok(log_line) = line {
+                let log: Log =
+                    serde_json::from_str(&log_line).expect("JSON was not well-formatted");
+                if let Err(e) = writeln!(writer, "{}", log.text.trim()) {
+                    println!("Writing error: {}", e.to_string());
+                }
+            }
+        }
+    }
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
